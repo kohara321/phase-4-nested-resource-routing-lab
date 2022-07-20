cbfa972c5fbae42db97c19a784311c3d76a5d2a9
Kaegan O'Hara <kohara321@gmail.com>
2022-07-20 11:06:19 -0600

    commit tree 8c54c3008c6d5f0ea8fe0e670f9427f344f751fa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
   def show
-    user = User.find_by(id: params[:id])
+    if params[:items_id]
+      items = Item.find(params[:items_id])
+      user = items.user
+    else
+      user = User.find_by(id: params[:id])
+    end
     render json: user, include: :items
   end
+
+private 
+
+def resource_not_found
+  render json: { error: "User not found" }, status: :not_found
+end
 
 end
